c36e800aa89db8956503b7adee054461c82ce8b1
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-09-20 00:41:44 +0900

    Update WhatToEatController.java

--- hw1/src/main/java/com/pard/server/hw1/WhatToEatController.java
+++ hw1/src/main/java/com/pard/server/hw1/WhatToEatController.java
@@ -27,11 +27,11 @@ public class WhatToEatController {
         return meals;
     }
 
-    // GET random recommendation http://localhost:8080/food/random
+    // GET random recommendation http://localhost:8080/food/recommend
     @GetMapping("/recommend")
     public String getRandomMeal() {
         if (meals.isEmpty()) {
-            return "추가하고 돌려.. 아무것도 없는데 어케 추천함!!!!";
+            return "뭐 추가하고 돌려.. 아무것도 없는데 어케 추천함!!!!";
         }
         Random random = new Random();
         return "오저는: " + meals.get(random.nextInt(meals.size()));
@@ -41,24 +41,24 @@ public class WhatToEatController {
     @PostMapping("/add")
     public String addMeal(@RequestParam String meal) {
         meals.add(meal);
-        return "Added meal: " + meal;
+        return meal + " 추가됨!";
     }
 
-    // PUT update a meal by index http://localhost:8080/food/update/0?newMeal=굶어라그냥
+    // PUT update a meal by index http://localhost:8080/food/update/2?newMeal=굶어라그냥
     @PutMapping("/update/{index}")
     public String updateMeal(@PathVariable int index, @RequestParam String newMeal) {
         if (index < 0 || index >= meals.size()) {
-            return "찾는 번호 없음..;;";
+            return index + "번은 없음..;;";
         }
         meals.set(index, newMeal);
-        return  index + newMeal + " 으로바꿈!";
+        return  index + "번 " + newMeal + " 으로바꿈!";
     }
 
-    // DELETE a meal by index http://localhost:8080/food/delete/1
+    // DELETE a meal by index http://localhost:8080/food/delete/2
     @DeleteMapping("/delete/{index}")
     public String deleteMeal(@PathVariable int index) {
         if (index < 0 || index >= meals.size()) {
-            return "번호클림..;;";
+            return index + "번은 없음..;;";
         }
         String removedMeal = meals.remove(index);
         return removedMeal + " 지웠다~" ;
