e8c220c3709d617b81efd20ea4b58b91f6d15cd1
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-10-24 01:38:55 +0900

    Update main components

---
+++ seminar4th/src/main/java/com/pard/server/seminar4th/user/dto/UserResponse.java
@@ -0,0 +1,38 @@
+package com.pard.server.seminar4th.user.dto;
+
+import com.pard.server.seminar4th.book.dto.BookResponse;
+import com.pard.server.seminar4th.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class UserResponse {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor @AllArgsConstructor
+
+    public static class ReadUser {
+        private String name;
+        private List<BookResponse.BookReadResponse> books;
+
+        public static ReadUser from(User user) {
+            return new ReadUser(user.getName(),
+                    BookResponse.BookReadResponse.bookToDto(user.getBook()));
+
+        }
+
+        //How it is written using builder ( does the same thing as the code above
+        public static ReadUser of(User user) {
+            return ReadUser.builder()
+                   .name(user.getName())
+                    .books(BookResponse.BookReadResponse.bookToDto(user.getBook()))
+                   .build();
+        }
+
+    }
+
+}
